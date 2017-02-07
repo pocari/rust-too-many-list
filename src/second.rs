b6bd4a2759f3aa2ff4ba10b31812ffb3dba738bb
pocari <caffelattenonsugar@gmail.com>
2017-02-07 17:35:04 +0900

    3.1 done

---
+++ src/second.rs
@@ -0,0 +1,81 @@
+pub struct List {
+    head: Link,
+}
+
+type Link = Option<Box<Node>>;
+
+struct Node {
+    elem: i32,
+    next: Link,
+}
+
+impl List {
+    pub fn new() -> Self {
+        List { head: None }
+    }
+
+    pub fn push(&mut self, elem: i32) {
+        let new_node = Box::new(Node {
+            elem: elem,
+            next: self.head.take(),
+        });
+        self.head = Some(new_node);
+    }
+
+    pub fn pop(&mut self) -> Option<i32> {
+        self.head.take().map(|boxed_node| {
+            let node = *boxed_node;
+            self.head = node.next;
+            node.elem
+        })
+    }
+}
+
+impl Drop for List {
+    fn drop(&mut self) {
+        let mut cur_link = self.head.take();
+        while let Some(mut boxed_node) = cur_link {
+            cur_link = boxed_node.next.take();
+        }
+    }
+}
+
+#[cfg(test)]
+mod test {
+    use super::List;
+
+    #[test]
+    fn basics() {
+        let mut list = List::new();
+
+        assert_eq!(list.pop(), None);
+
+        list.push(1);
+        list.push(2);
+        list.push(3);
+
+        assert_eq!(list.pop(), Some(3));
+        assert_eq!(list.pop(), Some(2));
+
+        list.push(4);
+        list.push(5);
+
+        assert_eq!(list.pop(), Some(5));
+        assert_eq!(list.pop(), Some(4));
+        assert_eq!(list.pop(), Some(1));
+
+        assert_eq!(list.pop(), None);
+    }
+
+    #[test]
+    fn stack_overflow_check() {
+        // ListのDropトレイトが無い状態でギリギリstack overflowが発生する状態にしてから
+        // Dropトレイトを実装するとstack overflowが起こらなくなる（=drop時に再帰が発生せずにスタックが
+        let mut list = List::new();
+
+        for i in 0..26177 {
+            list.push(i);
+        }
+        assert_eq!(list.pop(), Some(26176));
+    }
+}
