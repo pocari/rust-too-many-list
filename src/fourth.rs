7768da1f45ce0186925bd73d315edf42467d9f22
pocari <caffelattenonsugar@gmail.com>
2017-02-14 18:39:25 +0900

    5.5 done

--- src/fourth.rs
+++ src/fourth.rs
@@ -1,5 +1,5 @@
 use std::rc::Rc;
-use std::cell::{RefCell, Ref};
+use std::cell::{RefCell, Ref, RefMut};
 
 pub struct List<T> {
     head: Link<T>,
@@ -47,6 +47,21 @@ impl<T> List<T> {
         }
     }
 
+    pub fn push_back(&mut self, elem: T) {
+        let new_tail = Node::new(elem);
+        match self.tail.take() {
+            Some(old_tail) => {
+                old_tail.borrow_mut().next = Some(new_tail.clone());
+                new_tail.borrow_mut().prev = Some(old_tail);
+                self.tail = Some(new_tail);
+            }
+            None => {
+                self.head = Some(new_tail.clone());
+                self.tail = Some(new_tail);
+            }
+        }
+    }
+
     pub fn pop_front(&mut self) -> Option<T> {
         self.head.take().map(|old_head| {
             match old_head.borrow_mut().next.take() {
@@ -62,8 +77,35 @@ impl<T> List<T> {
         })
     }
 
+    pub fn pop_back(&mut self) -> Option<T> {
+        self.tail.take().map(|old_tail| {
+            match old_tail.borrow_mut().prev.take() {
+                Some(new_tail) => {
+                    new_tail.borrow_mut().next.take();
+                    self.tail = Some(new_tail);
+                }
+                None => {
+                    self.head.take();
+                }
+            }
+            Rc::try_unwrap(old_tail).ok().unwrap().into_inner().elem
+        })
+    }
+
     pub fn peek_front(&self) -> Option<Ref<T>> {
         self.head.as_ref().map(|node| Ref::map(node.borrow(), |node| &node.elem))
+    }
+
+    pub fn peek_back(&self) -> Option<Ref<T>> {
+        self.tail.as_ref().map(|node| Ref::map(node.borrow(), |node| &node.elem))
+    }
+
+    pub fn peek_front_mut(&self) -> Option<RefMut<T>> {
+        self.head.as_ref().map(|node| RefMut::map(node.borrow_mut(), |node| &mut node.elem))
+    }
+
+    pub fn peek_back_mut(&self) -> Option<RefMut<T>> {
+        self.tail.as_ref().map(|node| RefMut::map(node.borrow_mut(), |node| &mut node.elem))
     }
 }
 
@@ -96,6 +138,7 @@ mod test {
     fn peek() {
         let mut list: List<i32> = List::new();
         assert!(list.peek_front().is_none());
+        assert!(list.peek_back().is_none());
 
         list.push_front(1);
         list.push_front(2);
@@ -104,8 +147,25 @@ mod test {
         assert_eq!(&*list.peek_front().unwrap(), &3);
         assert_eq!(&*list.peek_front().unwrap(), &3);
 
+        assert_eq!(&*list.peek_back().unwrap(), &1);
+        assert_eq!(&*list.peek_back().unwrap(), &1);
+
         list.pop_front();
 
         assert_eq!(&*list.peek_front().unwrap(), &2);
+        assert_eq!(&*list.peek_back().unwrap(), &1);
+
+        list.pop_back();
+        assert_eq!(&*list.peek_front().unwrap(), &2);
+        assert_eq!(&*list.peek_back().unwrap(), &2);
+
+        list.push_back(5);
+        list.push_back(6);
+
+        assert_eq!(&*list.peek_front().unwrap(), &2);
+        assert_eq!(&mut *list.peek_front_mut().unwrap(), &mut 2);
+
+        assert_eq!(&*list.peek_back().unwrap(), &6);
+        assert_eq!(&mut *list.peek_back_mut().unwrap(), &mut 6);
     }
 }
